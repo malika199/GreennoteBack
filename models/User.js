04af4b6784ef98364ea77711c08548c10ffa001a
Elyes-nk <elyesnk@gmail.com>
2022-10-13 16:25:22 +0200

    Jwt

--- models/User.js
+++ models/User.js
@@ -1,33 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    
-    username: {
-        type: String,
-        required: true,
-      
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-
-   
-    profilPecture: {
-        type: String,
-        default: false,
-    },
-
-    subscription: {
-        type: Date
-       
-    },
-   
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  profilPecture: {
+    type: String,
+    default: false,
+  },
+  subscription: {
+    type: Date,
+  },
+  isActivate: {
+    type: Boolean,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
