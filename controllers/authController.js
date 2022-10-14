d9786d8903fb4c475a44b70d72961e574bc95133
Elyes-nk <elyesnk@gmail.com>
2022-10-14 15:25:16 +0200

    Fixe register fun

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,22 +3,26 @@ const jwt = require("jsonwebtoken");
 
 const CryptoJS = require("crypto-js");
 exports.register = (req, res) => {
-  let hashedPassword = CryptoJS.AES.encrypt(
-    req.body.password,
-    process.env.ACCESS_TOKEN_SECRET
-  ).toString();
-  console.log(req.body);
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    profilPecture: req.body.profilPecture,
-    subscription: req.body.subscription,
+  try {
+    let hashedPassword = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.ACCESS_TOKEN_SECRET
+    ).toString();
 
-    isActivate: false,
-    password: hashedPassword,
-  });
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      profilPecture: req.body.profilPecture,
+      subscription: req.body.subscription,
 
-  user.save().then((user) => res.status(200).json(user));
+      isActivate: false,
+      password: hashedPassword,
+    });
+
+    user.save().then((user) => res.status(200).json(user));
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 exports.login = async (req, res) => {
