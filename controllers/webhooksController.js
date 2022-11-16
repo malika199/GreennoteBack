d5285f1ba011f6c76f0b5f282c9837d2f1843441
Elyes-nk <elyesnk@gmail.com>
2022-11-16 22:43:59 +0100

    Debug stripe

--- controllers/webhooksController.js
+++ controllers/webhooksController.js
@@ -7,7 +7,8 @@ exports.stripewebhook = (req, res) => {
   let data;
   let eventType;
   const webhookSecret = process.env.WEBHOOK_SECRET_KEY;
-
+  // eslint-disable-next-line
+  console.log("## webhookSecret:", webhookSecret);
   if (webhookSecret) {
     let event;
     let signature = req.headers["stripe-signature"];
@@ -28,9 +29,12 @@ exports.stripewebhook = (req, res) => {
     data = req.body.data;
     eventType = req.body.type;
   }
-  console.log(eventType);
+  // eslint-disable-next-line
+  console.log("## eventType:", eventType);
   switch (eventType) {
     case "payment_intent.succeeded":
+      // eslint-disable-next-line
+      console.log('update user....')
       const updateUser = async () => {
         try {
           await User.findByIdAndUpdate(
