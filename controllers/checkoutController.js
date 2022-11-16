69b566aaf53821aac8240b2707431674c2892e40
Elyes-nk <elyesnk@gmail.com>
2022-11-16 21:05:01 +0100

    debug

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,44 +1,47 @@
 require("dotenv").config();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const initiateStripeSession = async (req) => {
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [{
-          price_data: {
-            currency: "eur",
-            product_data: {
-              name: req.body.subscribtion.name
-            },
-            unit_amount: (req.body.subscribtion.amount * 100).toFixed(0),
+  // eslint-disable-next-line
+  console.log("## req:", req);
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "eur",
+          product_data: {
+            name: req.body.subscribtion.name,
           },
-          quantity: 1
-        }],
-        payment_intent_data:{
-          metadata:{
-            userId:req.body.userId, 
-          },
+          unit_amount: (req.body.subscribtion.amount * 100).toFixed(0),
         },
-        mode: "payment",
-        success_url: `${process.env.CLIENT_URL}/`,
-        cancel_url: `${process.env.CLIENT_URL}/`,
-      });
+        quantity: 1,
+      },
+    ],
+    payment_intent_data: {
+      metadata: {
+        userId: req.body.userId,
+      },
+    },
+    mode: "payment",
+    success_url: `${process.env.CLIENT_URL}/`,
+    cancel_url: `${process.env.CLIENT_URL}/`,
+  });
 
-    return session;
-}
+  return session;
+};
 
 exports.createSession = async function (req, res) {
-    try {
-      const session = await initiateStripeSession(req);
-      res.status(200).json({
-        id: session.id,
-        price: session.amout_total,
-        currency: session.currency,
-      });      
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  };
+  try {
+    const session = await initiateStripeSession(req);
+    res.status(200).json({
+      id: session.id,
+      price: session.amout_total,
+      currency: session.currency,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
